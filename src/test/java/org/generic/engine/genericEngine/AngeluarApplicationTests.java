d83bbd0cb20b1ee83de846af62650dc3de0f4def
chitragarj <40055172+chitragarj@users.noreply.github.com>
2020-02-25 16:32:21 +0530

    Add files via upload

---
+++ src/test/java/org/generic/engine/genericEngine/AngeluarApplicationTests.java
@@ -0,0 +1,9 @@
+package com.angeluar.Angeluar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngeluarApplicationTests {
+
+}
