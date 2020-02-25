35838a607ce277ad36b6afac7161a37b4f89384d
chitragarj <40055172+chitragarj@users.noreply.github.com>
2020-02-25 16:20:54 +0530

    Add files via upload

---
+++ src/test/java/org/generic/engine/genericEngine/GenericEngineApplicationTests.java
@@ -0,0 +1,10 @@
+package src.test.java.org.generic.engine.genericEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericEngineApplicationTests {
+
+
+}
